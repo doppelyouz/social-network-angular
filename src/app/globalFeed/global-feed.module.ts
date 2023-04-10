1a0f04ed92acf998b1f294ea407c3051d6366065
anikisana <younker12345@gmail.com>
2023-04-10 13:50:50 +0600

    Добавляем вспомогательные компоненты

--- src/app/globalFeed/global-feed.module.ts
+++ src/app/globalFeed/global-feed.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { GlobalFeedComponent } from './components/global-feed/global-feed.component';
 import { RouterModule } from '@angular/router';
 import { FeedModule } from '../shared/modules/feed/feed.module';
+import { BannerModule } from '../shared/modules/banner/banner.module';
 
 const routes = [
   {
@@ -16,7 +17,8 @@ const routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    FeedModule
+    FeedModule,
+    BannerModule
   ]
 })
 export class GlobalFeedModule { }
--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -7,11 +7,20 @@ import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/reducers';
 import { FeedService } from './services/feed.service';
 import { RouterModule } from '@angular/router';
+import { ErrorMessageModule } from '../error-message/error-message.module';
+import { LoadingModule } from '../loading/loading.module';
 
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect]), StoreModule.forFeature('feed', reducers), RouterModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
+    RouterModule,
+    ErrorMessageModule,
+    LoadingModule
+  ],
   exports: [FeedComponent],
   providers: [FeedService]
 })
