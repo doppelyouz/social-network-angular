fe5f47e3b8e40990ccb2ecab1c4a88857abad34d
anikisana <younker12345@gmail.com>
2023-04-11 09:35:27 +0600

    Реализуем модуль пагинации. Часть 1

--- src/apiUrl.ts
+++ src/apiUrl.ts
@@ -1,1 +1,3 @@
 export const apiUrl = `https://conduit.productionready.io/api`;
+
+export const limit = 10;
--- src/app/shared/modules/feed/components/feed.component.ts
+++ src/app/shared/modules/feed/components/feed.component.ts
@@ -1,24 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { getFeedAction } from '../store/actions/getFeed.action';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { GetFeedResponseInterface } from '../types/getFeedResponse.interface';
 import { errorSelector, feedSelector, isLoadingSelector } from '../store/selectors';
+import { limit } from 'src/apiUrl';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps: string
 
   isLoading$: Observable<boolean>
   error$: Observable<string | null>
   feed$: Observable<GetFeedResponseInterface | null>
 
+  limit = limit
+  baseUrl: string
+  queryParamsSubscription: Subscription
+  currentPage: number
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.initializeValues()
@@ -28,9 +34,21 @@ export class FeedComponent implements OnInit {
   fetchData():void {
     this.store.dispatch(getFeedAction({url: this.apiUrlProps}));
   }
+
   initializeValues():void {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.feed$ = this.store.pipe(select(feedSelector));
+    this.baseUrl = this.router.url.split('?')[0]
+  }
+
+  initializeListeners():void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe((params:Params) => {
+      this.currentPage = Number(params['page'] || '1')
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.queryParamsSubscription.unsubscribe()
   }
 }
